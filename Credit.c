cc7973d22f529f575f1f1e10a8564bd379f9818f
Gopa Vasanth <gopavasanth1999@gmail.com>
2018-04-30 15:37:09 +0000

    Few Modifications

--- Credit.c
+++ Credit.c
@@ -6,28 +6,24 @@ int i,a,k,p,e;
 string n;
 k=0;
 n = get_string();
-    for (i=0;i<strlen(n);i++)
-    if (i%2!=0)
-    {
+    for (i=0;i<strlen(n);i++){
+    if (i%2!=0){
     k=k+atoi(n[i])*2;
 
-    if(atoi(n[i])*>=10)
-    {
+    if(atoi(n[i])*>=10){
     k=k-9;
     }
-    if(i%2==0)
-    {
+    if(i%2==0){
        p=p+atoi(n[i]);
     }
     e = p+k;
-    if (e%10==0)
-       {
+    if (e%10==0){
        printf("AMEX");
        }
-    else
-       {
+    else{
        printf("Invalid");
        }
     }
+}
 return 0;
     }
