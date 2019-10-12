fe33955ac4fac4eee103dc64d92c864d4bc1fc5a
sandeepdvn <56466444+sandeepdvn@users.noreply.github.com>
2019-10-12 18:45:11 +0530

    Create Water.cpp

---
+++ Water.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (void)
+{
+    int Minutes,i;
+    cout<<"Minutes:";
+    Minutes = GetInt();
+    i = Minutes*12;
+    cout<<i;
+    return 0;
+}
